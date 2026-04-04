091485f07bd7eea20d87bd5785ff62b96b260191
cechjan <cechjan.c@seznam.cz>
2026-04-04 20:52:16 +0200

    feat: added morphological transformations

---
+++ src/augmentations/__init__.py
@@ -0,0 +1,5 @@
+from .compose import Compose
+from .morph_ops import RandomMorphology
+from .gaussian_blur import GaussianBlur
+
+__all__ = ["Compose", "RandomMorphology", "GaussianBlur"]
---
+++ src/augmentations/morph_ops.py
@@ -0,0 +1,49 @@
+import random
+import numpy as np
+import cv2 as cv
+
+from augmentations.base import Transform
+
+
+class RandomMorphology(Transform):
+    """
+    Apply a random morphological transformation to an image.
+
+    This transformation randomly applies either **erosion** or **dilatation**
+    to the input image using a square kernel of randomly chosen odd size.
+
+    :param probability: Probability of applying the transformation.
+    :param kernel_size_range: Minimum and maximum kernel sizes for
+                              the morphological operations. Must be
+                              odd integers.
+    """
+
+    def __init__(self, probability: float = 1.0, kernel_size_range : tuple[int, int] = (1, 3)) -> None:
+        """
+        Initialize the random morphological transformation.
+
+        :param probability: Probability that the transformation will be applied when calling the transformation.
+        :param kernel_size_range: Tuple indicating the min and max size of the square kernel.
+        """
+        super().__init__(probability)
+        self.kernel_size_range = kernel_size_range
+
+    def apply(self, image: np.ndarray) -> np.ndarray:
+        """
+        Apply a random erosion or dilatation on the input image.
+
+        Since these operations do the opposites, one operation will be chosen
+        for the transformation. The kernel size is randomly selected from the
+        range and is guaranteed to be an odd integer.
+
+        :param image: Input image to apply the morphological transformation to.
+        """
+        k = random.randrange(self.kernel_size_range[0], self.kernel_size_range[1] + 1, 2)
+        kernel = np.ones((k, k), np.uint8)
+
+        option = random.choice(["erosion", "dilatation"])
+        
+        if option == "erosion":
+            return cv.erode(image, kernel, iterations=1)
+        else:
+            return cv.dilate(image, kernel, iterations=1)

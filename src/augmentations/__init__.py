ffd6c6303f3974f7f34d76c60a1318c62dd09bca
cech-matej <cech-matej@seznam.cz>
2026-04-04 21:21:45 +0200

    feat: add contrast transformation

--- src/augmentations/__init__.py
+++ src/augmentations/__init__.py
@@ -1,5 +1,6 @@
+from .color import RandomBrightness, RandomContrast
 from .compose import Compose
 from .morph_ops import RandomMorphology
 from .gaussian_blur import GaussianBlur
 
-__all__ = ["Compose", "RandomMorphology", "GaussianBlur"]
+__all__ = ["Compose", "RandomMorphology", "GaussianBlur", "RandomBrightness", "RandomContrast"]
--- src/augmentations/color.py
+++ src/augmentations/color.py
@@ -18,7 +18,7 @@ class RandomBrightness(Transform):
         :param probability: Probability of applying the brightness change.
         :param brightness: Maximum brightness change (positive of negative).
         """
-        
+
         super().__init__(probability)
         self.brightness = brightness
 
@@ -32,3 +32,33 @@ class RandomBrightness(Transform):
 
         beta = random.uniform(-self.brightness, self.brightness)
         return cv.convertScaleAbs(image, alpha=1.0, beta=beta)
+    
+
+class RandomContrast(Transform):
+    """
+    Randomly adjust the contrast of the image with a given probability.
+
+    Helps simulate variations in per darkness, scan quality, or camera exposure.
+    """
+
+    def __init__(self, probability: float = 1.0, contrast: float = 70.0) -> None:
+        """
+        Initialize the RandomContrast transformation.
+
+        :param probability: Probability of applying the contrast change.
+        :param contrast: Maximum contrast change as a percentage.
+        """
+        
+        super().__init__(probability)
+        self.contrast = contrast
+
+    def apply(self, image: np.ndarray) -> np.ndarray:
+        """
+        Apply random contrast adjustment.
+
+        :param image: Input image (NumPy array) to adjust.
+        :return: Contrast-adjusted image as a NumPy array.
+        """
+        
+        alpha = 1.0 + random.uniform(-self.contrast / 100, self.contrast / 100)
+        return cv.convertScaleAbs(image, alpha=alpha, beta=0)

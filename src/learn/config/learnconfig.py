9c3e29ad8bd6350e19c0ba73403748ba05f5f6c5
SmeysyaHromche <myrkykhta@gmail.com>
2026-04-11 18:15:14 +0200

    feat: learn config

---
+++ src/learn/config/learnconfig.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+class LearnDataConfig(BaseModel):
+    path_to_trn_meta_db: str
+    path_to_vld_meta_db: str
+    path_to_tst_meta_db: str
+    path_to_db: str
+    image_target_height: int
+    batch_size: int
+
+
+class LearnTrainConfig(BaseModel):
+    num_of_epochs: int
+    save_model_per_epoch: bool
+    output_model_dir: str
+
+
+class LearnModelConfig(BaseModel):
+    d_model: int
+    nhead: int
+    num_layers: int
+    dim_feedforward: int
+    dropout: float
+    max_seq_len: int
+
+
+class LearnConfig(BaseModel):
+    data: LearnDataConfig
+    train: LearnTrainConfig
+    model: LearnModelConfig
+
+
+# Example
+#print(LearnConfig.model_validate_json(open("learnconfig.json").read()))
+

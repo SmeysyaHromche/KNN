8e30c0de1b07a7334d1f3498dc7ec48b536c3218
SmeysyaHromche <myrkykhta@gmail.com>
2026-04-20 20:48:43 +0200

    feat: visual adapter

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,4 +2,4 @@ from .visualtokenizer import VisualTokenizer
 from .decoderonly import DecoderOnly
 from .knn import Knn
 
-__all__ = ["VisualTokenizer", "DecoderOnly", "Knn"]
+__all__ = ["VisualTokenizer", "VisualAdapter", "DecoderOnly", "Knn"]
